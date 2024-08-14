98482b49290b63433fb0417c9859cb88ba2dc774
Sara Romao <sara-romao@live.com>
2024-08-14 11:38:53 -0300

    feat: add tabela cripto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,73 @@
 
+import { BsSearch } from 'react-icons/bs';
 import styles from './home.module.css';
+import { Link } from 'react-router-dom';
 
 export function Home(){
 
 
     return(
-        <div>
-            <h1>Pagina 1 home</h1>
-        </div>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input 
+                    type="text"
+                    placeholder='Digite o nome da moeda... Ex bitcoin'
+                
+                />
+
+                <button type='submit'>
+                    <BsSearch size={30} color='#fff' />
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Moeda</th>
+                        <th scope="col">Valor Mecardo</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Volume</th>
+                        <th scope="col">Mudança</th>
+                    </tr>
+                </thead>
+                <tbody id='tbody'>
+                    <tr className={styles.tr}>
+                        
+                        <td className={styles.tdLabel} data-label="Moeda">
+                           <div className={styles.name}>
+                                <Link to={"/detail/bitcoin"}>
+                                    <span> Bitcoin</span> | BTC
+                                </Link>
+                           </div>
+                           
+                        </td>
+
+                        <td className={styles.tdLabel} data-label="Valor Mercado">
+                           1T
+                           
+                        </td>
+
+                        <td className={styles.tdLabel} data-label="Preço">
+                           60.000
+                           
+                        </td>
+
+                        <td className={styles.tdLabel} data-label="Volume">
+                           2B
+                           
+                        </td>
+
+                        <td className={styles.tdProfit} data-label="Mudança 24h">
+                           <span>1.20</span>
+                           
+                        </td>
+
+
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </main>
     )
 }
